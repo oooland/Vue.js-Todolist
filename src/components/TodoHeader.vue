1db76aac5385c93f2ea3232a7bf431bdd109fc87
oooland <positivehyemi97@gmail.com>
2018-09-04 19:48:08 +0900

    adding files

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <img src="./images/todoLogo.png" width="180" />
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
